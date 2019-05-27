a5867799c966a2e8cd72073cdeecbb4d96a0a3e2
cj dagostino <cjd.dagostino@gmail.com>
2019-05-27 11:39:55 -0600

    stars and booking fixed tests

---
+++ client/components/booking.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import BookingFixed from './bookingFixed.jsx';
+import Stars from './Stars.jsx';
+
+describe('Booking Fixed Banner', () => {
+
+  it('displays data properly', () => {
+    const fakeData = {
+      type: 'townhouse',
+      cost: 189
+    };
+    const wrapper = shallow(<BookingFixed listingData={fakeData} />);
+
+    expect(wrapper.find('#listingType').text()).toEqual('Entire townhouse');
+    expect(wrapper.find('#listingCost').text()).toEqual('$189 / night');
+  });
+
+  it('displays proper number of stars for listing', () => {
+    const wrapper = shallow(<Stars starCount={5} />);
+
+    expect(wrapper.children().length).toEqual(5);
+  });
+});
---
+++ client/components/stars.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import _ from 'underscore';
+
+const StarBox = styled.div`
+  display: inline-block;
+  font-size: 10px;
+  color: #914669;
+  margin: 0 0.25rem 0.5rem 0;
+`;
+
+const Stars = ({ starCount }) => {
+
+  return (
+    <StarBox>
+      {_.map(_.range(starCount), (k) => <i key={k} className="fas fa-star"></i>)}
+    </StarBox>
+  );
+};
+
+export default Stars;
