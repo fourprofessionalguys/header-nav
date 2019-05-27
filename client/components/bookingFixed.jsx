d49b33e622c30be79c23aa9d7d26ea94c7a9d2d1
cj dagostino <cjd.dagostino@gmail.com>
2019-05-27 11:58:32 -0600

    fixed import statment in banner component

--- client/components/bookingFixed.jsx
+++ client/components/bookingFixed.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Stars from './Stars.jsx';
+import Stars from './stars.jsx';
 
 const FixedBottom = styled.div`
   display: relative;
